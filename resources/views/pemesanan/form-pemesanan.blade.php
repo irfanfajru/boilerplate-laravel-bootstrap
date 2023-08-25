bd1383a0106499bd5e2bb3a0c41a6fc6c125ca40
Farrah Dillah  Angeli <farrahdillahangeli@gmail.com>
2023-08-25 21:39:22 +0700

    update view

--- resources/views/pemesanan/form-pemesanan.blade.php
+++ resources/views/pemesanan/form-pemesanan.blade.php
@@ -121,6 +121,7 @@
     <script src="https://code.jquery.com/jquery-3.7.0.slim.js" integrity="sha256-7GO+jepT9gJe9LB4XFf8snVOjX3iYNb0FHYr5LI1N5c=" crossorigin="anonymous"></script>
     <script>
         function hitungTotal(dataKamar) {
+            //ambil nilai
             durasi = $("#durasi").val()
             breakfast = $("#breakfast").is(":checked")
             hargaKamar = 0
--- resources/views/pemesanan/invoice.blade.php
+++ resources/views/pemesanan/invoice.blade.php
@@ -27,17 +27,41 @@
     @include('components.navbar')
 
     <!-- content -->
-    <div class="p-10 space-y-10">
-        <!-- hotel -->
-        <h1>Pemesanan anda berhasil</h1>
-        <p>Nama Pemesan : {{$pemesanan->nama}}</p>
-        <p>Nomor Identitas : {{$pemesanan->no_identitas}}</p>
-        <p>Jenis Kelamin : {{$pemesanan->jenis_kelamin}}</p>
-        <p>Tipe Kamar : {{$pemesanan->hotel->tipe}}</p>
-        <p>Durasi Penginapan : {{$pemesanan->durasi}} Hari</p>
-        <!-- kalo ada diskon -->
-        <p>Discount : {{$pemesanan->durasi>3? "10%":"0%"}} </p>
-        <p>Total Bayar : {{number_format($pemesanan->total,0,',','.');}}</p>
+    
+    <div class="p-20">
+        <div class="p-10 space-y-10 mx-auto max-w-md border rounded bg-[#F8F8F8] shadow">
+            <!-- hotel -->
+            <h1 class="text-2xl font-bold text-center">Pemesanan Anda Berhasil</h1>
+            <p class="font-bold">Detail Pemesan:</p>
+            <div class="flex">
+                <p class="w-1/2">Nama Pemesan</p>
+                <p class="w-1/2">: {{$pemesanan->nama}}</p>
+            </div>
+            <div class="flex">
+                <p class="w-1/2">Nomor Identitas</p>
+                <p class="w-1/2">: {{$pemesanan->no_identitas}}</p>
+            </div>
+            <div class="flex">
+                <p class="w-1/2">Jenis Kelamin</p>
+                <p class="w-1/2">: {{$pemesanan->jenis_kelamin}}</p>
+            </div>
+            <div class="flex">
+                <p class="w-1/2">Tipe Kamar</p>
+                <p class="w-1/2">: {{$pemesanan->hotel->tipe}}</p>
+            </div>
+            <div class="flex">
+                <p class="w-1/2">Durasi Penginapan</p>
+                <p class="w-1/2">: {{$pemesanan->durasi}} Hari</p>
+            </div>
+            <div class="flex">
+                <p class="w-1/2">Discount</p>
+                <p class="w-1/2">: {{$pemesanan->durasi > 3 ? "10%" : "0%"}}</p>
+            </div>
+            <div class="flex">
+                <p class="w-1/2">Total Bayar</p>
+                <p class="w-1/2">: Rp {{number_format($pemesanan->total, 0, ',', '.')}}</p>
+            </div>
+        </div>
     </div>
 </body>
 
